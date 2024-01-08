510273e7bf8f001385123d60c7a146fca9aae927
Xiong An <xiong.an@keysight.com>
2024-01-08 10:53:52 +0800

    Update

--- LanguageWithNewLine/src/main/CallMain.java
+++ LanguageWithNewLine/src/main/CallMain.java
@@ -1,13 +1,22 @@
 package main;
 
+import java.util.List;
+
 import parser.LanguageWithNewLineParser;
 import parser.ParseException;
+import parser.Token;
 
 public class CallMain {
 
 	public static void main(String[] args) {
         try {
             System.out.println(LanguageWithNewLineParser.evaluateAdd("2+4"));
+            List<List<Token>> tokenList = LanguageWithNewLineParser.evaluateLines("a,b,c\ncccc");
+            for (List<Token> tl : tokenList) {
+            	for (Token t : tl) {
+            		System.out.println(t.image);
+            	}
+            }
         }
         catch (ParseException ex) {
             System.err.println(ex.getMessage());
